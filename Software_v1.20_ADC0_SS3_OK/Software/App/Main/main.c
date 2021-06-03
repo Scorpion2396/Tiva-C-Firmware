b58c54c903f09aa4d2674908907bd49e32d087cc
sakshamraj96 <sakshamraj96@gmail.com>
2021-06-03 21:07:05 +0530

    ADC DRIVER Included Configured with ADC0 Sample sequencer 3

---
+++ Software_v1.20_ADC0_SS3_OK/Software/App/Main/main.c
@@ -0,0 +1,58 @@
+#include "TM4C123GH6PM.h"
+//#include "GPIOdriver.h"
+//#include "Timer.h"
+#include <stdio.h>
+
+#include "GPIO_Driver_Interface.h"
+#include "Timer_Interface.h"
+#include "ADC_Driver_Interface.h"
+#include "Common_Datatype.h"
+#include "Common_Macros.h"
+
+
+
+ int main()
+{
+
+
+DigitalWrite(PF4,HIGH);
+DigitalWrite(PD7,LOW); 
+DigitalWrite(PD6,HIGH);
+DigitalWrite(PC7,LOW);
+DigitalWrite(PC6,HIGH);
+DigitalWrite(PC5,LOW);
+  
+    volatile int val_1 = 0 ;
+    volatile int val_2 = 0 ;
+    volatile int val_3 = 0 ;
+    volatile int val_4 = 0 ;
+    volatile int val_5 = 0 ;
+    volatile int val_6 = 0 ;
+
+    
+  while(1)
+  {
+	
+   Timer_0();
+	
+   val_1 = analogRead(PE2);
+   val_2 = analogRead(PE1);
+   val_3 = analogRead(PD3);
+   val_4 = analogRead(PD2);
+   val_5 = analogRead(PD1);
+   val_6 = analogRead(PD0);
+    
+    if(Flag_1000ms == 1)
+    {
+        printf("PE2 = %d\n",val_1);
+        printf("PE1 = %d\n",val_2);
+        printf("PD3 = %d\n",val_3);
+        printf("PD2 = %d\n",val_4);
+        printf("PD1 = %d\n",val_5);
+        printf("PD0 = %d\n",val_6);
+        
+      		    
+    }
+
+  }
+}
---
+++ Software_v1.20_ADC0_SS3_OK/Software/Driver/GPIO/GPIOdriver.c
@@ -0,0 +1,122 @@
+#include "GPIOdriver.h"
+#include "TM4C123GH6PM.h"
+
+
+static uint8_t Port_IO_flag[6] = {(uint8_t)0};
+static uint8_t GPIO_Init_flag  = (uint8_t)0;
+GPIOA_Type* const PORT[6] = { GPIOA , GPIOB , GPIOC , GPIOD , GPIOE , GPIOF };
+
+/************************************* <THIS FUNCTION IS USE TO INITIALIZE DIGITAL INPUT/OUTPUT PORT> ************************************/
+void GPIO_Init()
+{ 
+	SYSCTL->RCGCGPIO = 0x3F;
+	
+    GPIOB->LOCK = 0x4C4F434B;
+    GPIOC->LOCK = 0x4C4F434B;
+    GPIOD->LOCK = 0x4C4F434B;
+    GPIOF->LOCK = 0x4C4F434B;
+       
+    GPIO_Init_flag = (uint8_t)1;  
+}
+
+/************************************************ <THIS FUNCTION IS USE FOR DIGITAL WRITE> **********************************************/
+void DigitalWrite(GPIO_PORT port_x , GPIO_HI_LO value)
+{
+	uint8_t port_num = ((port_x)/(uint8_t)10) ;
+	uint8_t pin_num  = ((port_x)%(uint8_t)10) ;
+        
+    uint8_t IO_flag = (Port_IO_flag[port_num] & (1<<(pin_num)));
+            IO_flag = (IO_flag >> pin_num);      
+                
+ 	//printf("out op_flag = %d\n",OP_flag);	
+    
+	// printf("out ip_flag = %d\n",IP_flag); 
+	
+	if(GPIO_Init_flag == 0)
+	{
+		GPIO_Init();
+	}
+	
+				
+	if(IO_flag == 0) 
+	{
+		PORT[port_num]->CR     |=  (1<<(pin_num));
+		//	PORT[port_num]->AFSEL  &= ~(1<<(pin_num));
+		PORT[port_num]->DIR    |=  (1<<(pin_num));
+	    PORT[port_num]->DEN    |=  (1<<(pin_num));
+	    
+	    Port_IO_flag[port_num] |= (1<<(pin_num));
+	}	
+			
+	else
+	{
+        ;
+	} 
+			
+	if(value == HIGH)   
+	{
+        PORT[port_num]->DATA  |=  (1<<(pin_num));
+	}
+    		
+    else
+    {
+    	PORT[port_num]->DATA  &=  ~(1<<(pin_num));
+	}
+
+}
+
+
+/************************************************ <THIS FUNCTION IS USE FOR DIGITAL READ> **********************************************/
+char DigitalRead(GPIO_PORT port_x , GPIO_PU_PD state)
+{
+		
+	uint8_t port_num = ((port_x) / (uint8_t)10) ;
+	uint8_t pin_num  = ((port_x) % (uint8_t)10) ;
+        
+    uint8_t IO_flag = (Port_IO_flag[port_num] & (1<<(pin_num)));
+            IO_flag = (IO_flag >> pin_num);
+                              
+ 	// printf("in op_flag = %d\n",OP_flag);	
+    // printf("in ip_flag = %d\n",IP_flag);
+	
+	__IO uint8_t Input_Data = 0U;
+	
+	if(GPIO_Init_flag == 0)
+	{
+		GPIO_Init();	
+	}
+
+
+	if(IO_flag == 0)
+	{
+		PORT[port_num]->CR     |=  (1<<(pin_num));
+	//	PORT[port_num]->AFSEL  &= ~(1<<(pin_num));				
+		PORT[port_num]->DIR    &= ~(1<<(pin_num));
+	    PORT[port_num]->DEN    |=  (1<<(pin_num));
+	    		
+	    Port_IO_flag[port_num] |= (1<<(pin_num));
+    }
+			
+	else
+	{
+        ;
+	} 
+			
+			
+	if( state == PU )
+	{
+		PORT[port_num]->PUR   |=  (1<<(pin_num));				
+	}
+	
+	else
+	{
+		PORT[port_num]->PDR   |=  (1<<(pin_num));				
+	}
+			 	
+    Input_Data  = PORT[port_num]->DATA;      
+	Input_Data &= (1 << (pin_num));
+	Input_Data  = (Input_Data >> (pin_num));
+                      
+return(Input_Data);
+    		
+}
